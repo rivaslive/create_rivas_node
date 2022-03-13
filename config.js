0394442aa0acfab9206ccfaa9567d0d67a6809c9
rivaslive <krivas@brandy.la>
2022-03-12 23:07:12 -0600

    Add crn-rest and add install dependencies

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const repositoriesUrl = {
-  "default-vite": "https://github.com/rivaslive/crn-graphql.git",
-  graphql: "https://github.com/rivaslive/crn-graphql.git",
-  rest: "https://github.com/rivaslive/crn-graphql.git",
+  'default-vite': 'https://github.com/rivaslive/crn-rest.git',
+  graphql: 'https://github.com/rivaslive/crn-graphql.git',
+  rest: 'https://github.com/rivaslive/crn-rest.git',
 };
 
 const questions = [
   {
-    type: "input",
-    name: "appName",
+    type: 'input',
+    name: 'appName',
     message: "Please enter your new project's name.",
   },
   {
-    type: "list",
-    name: "template",
-    message: "Select template for your api.",
-    choices: ["default-vite", "graphql", "rest"],
+    type: 'list',
+    name: 'template',
+    message: 'Select template for your api.',
+    choices: ['default-vite', 'graphql', 'rest'],
   },
 ];
 
--- index.js
+++ index.js
@@ -1,19 +1,49 @@
 #!/usr/bin/env node
-"use strict";
+'use strict';
 
-const inquirer = require("inquirer");
-const { spawnSync } = require("child_process");
-const { questions, repositoriesUrl } = require("./config");
+const inquirer = require('inquirer');
+const { spawnSync, spawn } = require('child_process');
+const { questions, repositoriesUrl } = require('./config');
 
-console.log("Esto lo reconoce");
 const createProject = ({ appName, template }) => {
-  const child = spawnSync("git", [
-    "clone",
+  const cloneRepo = spawnSync('git', [
+    'clone',
     repositoriesUrl[template],
     `${process.cwd()}/${appName}`,
   ]);
-  console.log(`${child.stderr}`);
-  console.log("🚀 Created project successfully");
+
+  if (cloneRepo.error) {
+    return console.log(`${child.stderr}`);
+  }
+
+  try {
+    process.chdir(`${process.cwd()}/${appName}`);
+    console.log(`cd ./${appName}`);
+
+    let packageManager = 'yarn';
+    const verifyYarnInstall = spawnSync('yarn', ['--version']);
+
+    if (verifyYarnInstall.error) {
+      packageManager = 'npm';
+    }
+
+    console.log(`installing devs using ${packageManager}`);
+    const installDeps = spawnSync(packageManager, ['install'], {
+      stdio: 'inherit',
+    });
+
+    if (installDeps.error) {
+      return console.log(`${installDeps.stderr}`);
+    }
+
+    console.log('🚀 Created project successfully');
+    console.log(`Access with cd ${appName}`);
+    return console.log(
+      `execute ${packageManager} ${packageManager !== 'yarn' ? 'run' : ''} dev`
+    );
+  } catch (error) {
+    return console.error(error);
+  }
 };
 
 const createQuestions = () => {
